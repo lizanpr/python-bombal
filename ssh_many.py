c57ca48a292b13f26863383962788fffc4551486
lizanpr <lizan.pr@gmail.com>
2017-06-25 13:59:18 -0400

    Update ssh_many.py

--- ssh_many.py
+++ ssh_many.py
@@ -9,10 +9,10 @@ from paramiko.ssh_exception import SSHException
 script, devices_file, commands_file = argv
 
 with open(devices_file) as f:
-        devices = [line.strip() for line in f]
+    devices = [line.strip() for line in f]
 
 with open(commands_file) as f:
-        commands = [line.strip() for line in f]
+    commands = [line.strip() for line in f]
 
 username = raw_input("Enter your username: ")
 password = raw_input("Enter your password: ")
@@ -43,18 +43,24 @@ for device in devices:
         fail.close()
 
     else:
-        fd = open('C:\config-' + device + '.txt','w')
-        # Make a copy of current sys.stdout (?)
+        # Backup current sys.stdout
         old_stdout = sys.stdout
-        # Redirect system output to the file object
-        sys.stdout = fd
+        # Class for redirecting stdout to both terminal and file
+        class Logger(object):
+            def __init__(self):
+                self.terminal = sys.stdout
+                self.log = open('C:\config-' + device + '.txt','w')
 
+            def write(self, message):
+                self.terminal.write(message)
+                self.log.write(message)
+
+        sys.stdout = Logger()
         # send_config_set enters and exits config mode automatically. Use 'do'
         # to execute exec-mode commands such as show
         output = net_connect.send_config_set(commands)
         print output
 
-        fd.close()
         # Restore sys.stdout with the backup
         sys.stdout = old_stdout
 
